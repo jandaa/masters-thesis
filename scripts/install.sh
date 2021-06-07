1f8be0e3357918e9eb7353dd6c60507e47594647
andrej <andrej.janda@gmail.com>
2021-06-07 16:51:25 -0400

    Adds cu111 to pytorch install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 python -m venv .venv
 source .venv/bin/activate
 
+# Install pytorch with CUDA version 11.1
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+
+# Install all other dependencies
 pip install -r requirements.dev
 pip install -r requirements.prod
 
