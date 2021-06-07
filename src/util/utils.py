bc4137fc304b17ef3ad83f28d9d3c450ef67d841
andrej <andrej.janda@gmail.com>
2021-06-07 16:52:32 -0400

    copies over necessary util function from PointGroup

---
+++ src/util/utils.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def get_batch_offsets(batch_idxs, bs):
+    """
+    :param batch_idxs: (N), int
+    :param bs: int
+    :return: batch_offsets: (bs + 1)
+    """
+    batch_offsets = torch.zeros(bs + 1).int().cuda()
+    for i in range(bs):
+        batch_offsets[i + 1] = batch_offsets[i] + (batch_idxs == i).sum()
+    assert batch_offsets[-1] == batch_idxs.shape[0]
+    return batch_offsets
