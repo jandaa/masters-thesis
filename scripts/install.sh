064e60b7e61beaef7e704d8b696506657191bea4
andrej <andrej.janda@gmail.com>
2021-06-07 10:49:49 -0400

    Adds first version of install script

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+python -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.dev
+pip install -r requirements.prod
+
+base_dir=$PWD
+
+# Clone spconv
+git clone git@github.com:traveller59/spconv.git src/lib/spconv --recursive
+cd src/lib/spconv
+git checkout fad3000249d27ca918f2655ff73c41f39b0f3127
+
+python setup.py bdist_wheel
+cd dist
+pip install *.whl
+
+cd $base_dir/src/lib/pointgroup_ops
+python setup.py develop
