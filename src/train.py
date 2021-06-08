548e19cf38448ba26f5eeb421c4c1857176e446b
andrej <andrej.janda@gmail.com>
2021-06-08 13:58:51 -0400

    Adds loading from checkpoint and running tests

--- src/train.py
+++ src/train.py
@@ -1,35 +1,51 @@
 import os
 import logging
+from pathlib import Path
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
+from util import utils
 
-import torch
-from torch import nn
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, random_split
-from torch.nn import functional as F
 
 from dataloaders.scannetv2 import ScannetDataModule
 from model.pointgroup import PointGroup
+
 
 log = logging.getLogger(__name__)
 
 
 @hydra.main(config_path="config", config_name="config")
 def semantics(cfg: DictConfig) -> None:
+
     # Print output config for debug purposes
     print(OmegaConf.to_yaml(cfg))
+
+    # Load from checkpoint if availble
+    if cfg.continue_from:
+        continue_from_dir = hydra.utils.get_original_cwd() + "/" + cfg.continue_from
+        utils.load_previous_training(Path(continue_from_dir), Path.cwd())
 
     # Try instanciating and loading Scannet
     scannet = ScannetDataModule(cfg)
     scannet.setup()
-    scannet.train_dataloader()
-    scannet.val_dataloader()
 
     model = PointGroup(cfg)
 
+    if cfg.continue_from:
+        model.load_from_checkpoint(
+            checkpoint_path=os.path.join(
+                hydra.utils.get_original_cwd(), "checkpoints/current.ckpt"
+            ),
+            cfg=cfg,
+        )
+
     trainer = pl.Trainer(gpus=1)
+
+    # run to test the output
+    # trainer.test(model, scannet.test_dataloader())
+
+    # Train model
     trainer.fit(model, scannet.train_dataloader(), scannet.val_dataloader())
 
 
