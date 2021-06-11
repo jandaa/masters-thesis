5ea75d921bc724c11a66f79a953456d03cb45c3a
Andrej <andrej.janda@mail.utoronto.ca>
2021-06-11 09:52:22 -0400

    updates script to use packages dir

--- scripts/third_party.sh
+++ scripts/third_party.sh
@@ -1,5 +1,5 @@
 
 # Clone spconv
-git clone git@github.com:traveller59/spconv.git src/lib/spconv --recursive
-cd src/lib/spconv
+git clone git@github.com:traveller59/spconv.git src/packages/spconv --recursive
+cd src/packages/spconv
 git checkout fad3000249d27ca918f2655ff73c41f39b0f3127
