2c395564763289ceab459e12b19b9e575652343b
Andrej <andrej.janda@mail.utoronto.ca>
2021-06-11 16:06:56 -0400

    Add training parameters and logging

--- src/train.py
+++ src/train.py
@@ -21,34 +21,39 @@ def semantics(cfg: DictConfig) -> None:
     # Print output config for debug purposes
     print(OmegaConf.to_yaml(cfg))
 
-    # Load from checkpoint if availble
-    if cfg.continue_from:
-        continue_from_dir = hydra.utils.get_original_cwd() + "/" + cfg.continue_from
-        utils.load_previous_training(Path(continue_from_dir), Path.cwd())
+    # # Load from checkpoint if availble
+    # if cfg.continue_from:
+    #     continue_from_dir = hydra.utils.get_original_cwd() + "/" + cfg.continue_from
+    #     utils.load_previous_training(Path(continue_from_dir), Path.cwd())
 
+    log.info("Loading data module")
     # Try instanciating and loading Scannet
     scannet = ScannetDataModule(cfg)
     scannet.setup()
-
+    
+    log.info("Loading model")
     model = PointGroup(cfg)
-
+    
     checkpoint_path = None
     if cfg.continue_from:
         # TODO: Write a function to select the latest checkpoint
         checkpoint_path = os.path.join(
             hydra.utils.get_original_cwd(), "checkpoints/epoch=15-step=31.ckpt"
         )
-    # model.load_from_checkpoint(
-    #     checkpoint_path=checkpoint_path,
-    #     cfg=cfg,
-    # )
 
-    trainer = pl.Trainer(gpus=1, resume_from_checkpoint=checkpoint_path)
+    log.info("Building trainer")
+    trainer = pl.Trainer(
+        gpus=1, 
+        resume_from_checkpoint=checkpoint_path, 
+        max_epochs=364,
+        check_val_every_n_epoch=10
+    )
 
     # run to test the output
     # trainer.test(model, scannet.test_dataloader())
 
     # Train model
+    log.info("starting training")
     trainer.fit(model, scannet.train_dataloader(), scannet.val_dataloader())
 
 
