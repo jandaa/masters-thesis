3537aee0e7f5f4c25f1e08d1a20ba79a319a4b28
andrej <andrej.janda@gmail.com>
2021-08-20 10:48:31 -0400

    update pip-download script

--- scripts/pip-download.sh
+++ scripts/pip-download.sh
@@ -1,4 +1,5 @@
 mkdir dist
 pip3 download antlr4-python3-runtime==4.8 -d dist/
 pip3 download --no-deps hydra-core omegaconf -d dist/
+pip3 download --no-deps torch-metrics pytorch-lightning==1.4.2 -d dist/
 
