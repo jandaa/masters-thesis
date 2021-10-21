c111104b33e72a373c04269f131d94afea74459b
andrej <andrej.janda@gmail.com>
2021-10-21 17:05:04 -0400

    update debug script

--- scripts/experiments/scannet/debug.sh
+++ scripts/experiments/scannet/debug.sh
@@ -1,10 +1,7 @@
-source .venv/bin/activate
 python src/train.py \
-    dataset_dir=/home/andrej/datasets/scannet \
+    dataset_dir=$dataset_dir \
     dataset=scannet \
     hydra.run.dir=outputs/scannetv2/debug \
     gpus=[0] \
     preload_data=False \
-    dataset.train_split_file=scannetv2-val-debug.txt \
-    dataset.val_split_file=scannetv2-val-debug.txt \
-    dataset.test_split_file=scannetv2-val-debug.txt \
+    model.train.train_workers=10
