9313f21226349f1d7c482f57116b7ec360cbab28
andrej <andrej.janda@gmail.com>
2021-10-22 08:45:48 -0400

    update semantic training scripts

--- scripts/experiments/scannet/semantic_baseline.sh
+++ scripts/experiments/scannet/semantic_baseline.sh
@@ -1,8 +1,9 @@
 python src/train.py \
     dataset_dir=$dataset_dir \
     dataset=scannet \
-    hydra.run.dir=outputs/scannetv2/baseline \
+    hydra.run.dir=outputs/scannetv2/baseline-deterministic \
     tasks=["train","eval"] \
     preload_data=False \
     gpus=1 \
+    max_epochs=120 \
     model.train.train_workers=10
