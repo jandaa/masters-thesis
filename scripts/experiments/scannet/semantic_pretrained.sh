1389ed502e83f634f4bd9985067920b4a0d3a9f9
andrej <andrej.janda@gmail.com>
2021-10-23 09:43:11 -0400

    update semantic scripts

--- scripts/experiments/scannet/semantic_pretrained.sh
+++ scripts/experiments/scannet/semantic_pretrained.sh
@@ -1,10 +1,10 @@
 python src/train.py \
     dataset_dir=$dataset_dir \
     dataset=scannet \
-    hydra.run.dir=outputs/scannetv2/pretrained-deterministic \
+    hydra.run.dir=outputs/scannetv2/pretrained\
     tasks=["train","eval"] \
     preload_data=False \
     gpus=1 \
     max_epochs=120 \
-    model.train.train_workers=10 \
+    check_val_every_n_epoch=10 \
     pretrain_checkpoint=\"epoch=119-step=2159-val_loss=8.57.ckpt\"
