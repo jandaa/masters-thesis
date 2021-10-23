270cf391910e4fe8d9bb16bf1f18940bacb2015b
andrej <andrej.janda@mail.utoronto.ca>
2021-10-23 11:02:55 -0400

    rename preprocess script and add output dir

--- scripts/experiments/scannet/preprocess.sh
+++ scripts/experiments/scannet/preprocess.sh
@@ -1,10 +1,12 @@
 export SCANNET_DIR=/media/starslab/datasets/scannet
+export OUTPUT_DIR=/media/starslab/datasets/scannet_full_preprocessed
 export HYDRA_FULL_ERROR=1
 
 preprocess() {
     filename=$1
     python src/preprocess.py \
         dataset_dir=$SCANNET_DIR \
+        output_dir=$OUTPUT_DIR \
         dataset=scannet \
         sens_file=$filename \
         force_reload=True
