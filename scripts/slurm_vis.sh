b179c453a1b08ffd2bde320f60a3ee16707fc936
andrej <andrej.janda@gmail.com>
2021-10-23 09:02:39 -0400

    update slurm tensorboard visualization

--- scripts/slurm_vis.sh
+++ scripts/slurm_vis.sh
@@ -1,1 +1,28 @@
-ssh -N -f -L localhost:6006:$1:$2 ajanda@beluga.computecanada.ca
+#!/bin/bash
+#SBATCH --cpus-per-task=10  # Cores proportional to GPUs: 6 on Cedar, 10 on Béluga, 16 on Graham.
+#SBATCH --mem=47000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=0-01:00:00     # DD-HH:MM:SS
+
+module load python/3.8
+module load sparsehash
+module load boost/1.72.0
+module load cuda/11.1
+
+module load StdEnv/2020  
+module load cudacore/.11.1.1
+module load cudnn/8.2.0
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip3 install torch==1.9.1 --no-index
+pip3 install  dist/*.tar.gz --no-index
+pip3 install  dist/*.whl --no-index
+
+# install each requirement individually incase some are unavailable
+cat setup/requirements.dev | xargs -n 1 pip3 install --no-index 
+cat setup/requirements.prod | xargs -n 1 pip3 install --no-index
+
+tensorboard --logdir=outputs
+
+# to connect to tensorboard
+# ssh -N -L localhost:6006:$1:$2 ajanda@beluga.computecanada.ca
