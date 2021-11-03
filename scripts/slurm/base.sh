244a3f1621843a29bc084ac34499d493de6fd483
andrej <andrej.janda@gmail.com>
2021-11-03 08:36:46 -0400

    updates slurm install scripts

--- scripts/slurm/base.sh
+++ scripts/slurm/base.sh
@@ -2,6 +2,7 @@
 
 module load python/3.8
 module load sparsehash
+module load openblas/0.3.17
 module load boost/1.72.0
 module load cuda/11.1
 
@@ -22,8 +23,28 @@ cat setup/requirements.prod | xargs -n 1 pip3 install --no-index
 # Run from the root of repository (e.g. sbatch scripts/slurm.sh)
 base_dir=$PWD
 
-cd $base_dir/src/packages/spconv
-python setup.py bdist_wheel
+# Install minkowki engine
+minkowski_dir=$base_dir/src/packages/MinkowskiEngine 
+is_wheel=$(find $minkowski_dir -name "*.whl" | wc -l)
+if [ $is_wheel -eq 0 ]
+then
+    cd $minkowski_dir
+    export CXX=c++;
+    python setup.py bdist_wheel --blas=openblas --force_cuda
+fi
+
+cd $base_dir/src/packages/MinkowskiEngine/dist
+pip3 install *.whl
+
+# Install spconv
+spconv_dir=$base_dir/src/packages/spconv
+is_wheel=$(find $spconv_dir -name "*.whl" | wc -l)
+if [ $is_wheel -eq 0 ]
+then
+    cd $spconv_dir
+    python setup.py bdist_wheel
+fi
+
 cd $base_dir/src/packages/spconv/dist
 pip3 install *.whl
 
--- scripts/third_party.sh
+++ scripts/third_party.sh
@@ -1,3 +1,6 @@
 # Clone spconv
 # git clone git@github.com:jandaa/spconv.git src/packages/spconv --recursive
 git clone https://github.com/outsidercsy/spconv.git src/packages/spconv --recursive
+
+# Clone Minkowski Engine for SLURM
+git clone  --depth 1 --branch v0.5.4 https://github.com/NVIDIA/MinkowskiEngine.git src/packages/MinkowskiEngine
