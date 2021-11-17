41b0f5109b5f2be9923b59238367e495400eb54f
andrej <andrej.janda@mail.utoronto.ca>
2021-11-17 09:04:41 -0500

    finish merge

--- scripts/experiments/scannet/minkowski-pretrain.sh
+++ scripts/experiments/scannet/minkowski-pretrain.sh
@@ -4,14 +4,6 @@ python src/train.py \
     model=minkowski \
     tasks=["pretrain"] \
     hydra.run.dir=outputs/scannetv2/minkowski-pretrained-2cm \
-<<<<<<< HEAD
-    gpus=[0] \
-    dataset.pretrain.batch_size=4 \
-    dataset.pretrain.accumulate_grad_batches=1 \
-    model.train.train_workers=8 \
-    dataset.scale=50 \
-    check_val_every_n_epoch=10 
-=======
     gpus=2 \
     dataset.pretrain.batch_size=16 \
     dataset.pretrain.accumulate_grad_batches=2 \
@@ -20,5 +12,4 @@ python src/train.py \
     check_val_every_n_epoch=10 \
     model.optimizer.type=SGD \
     model.optimizer.lr=0.1
->>>>>>> 7aeac3801e2b0b822e12319033d411f93af6a7bf
     # pretrain_checkpoint=last.ckpt
