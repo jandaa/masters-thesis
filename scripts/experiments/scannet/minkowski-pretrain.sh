27dd91c44347de78b82cf4cd886969ab1e8a9351
andrej <andrej.janda@mail.utoronto.ca>
2021-11-17 09:03:33 -0500

    updates scripts

--- scripts/experiments/scannet/minkowski-pretrain.sh
+++ scripts/experiments/scannet/minkowski-pretrain.sh
@@ -4,11 +4,10 @@ python src/train.py \
     model=minkowski \
     tasks=["pretrain"] \
     hydra.run.dir=outputs/scannetv2/minkowski-pretrained-2cm \
-    gpus=[0,1] \
-    dataset.pretrain.batch_size=16 \
-    dataset.pretrain.accumulate_grad_batches=2 \
-    model.train.train_workers=20 \
+    gpus=[0] \
+    dataset.pretrain.batch_size=4 \
+    dataset.pretrain.accumulate_grad_batches=1 \
+    model.train.train_workers=8 \
     dataset.scale=50 \
-    check_val_every_n_epoch=10 \
-    dataset.pretrain.max_epochs=500
+    check_val_every_n_epoch=10 
     # pretrain_checkpoint=last.ckpt
