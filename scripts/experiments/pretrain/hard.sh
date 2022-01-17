607879f27bb9d4deafe76eb20bd5827feb956461
andrej <andrej.janda@mail.utoronto.ca>
2022-01-17 12:26:21 -0500

    updates pretraining scripts

--- scripts/experiments/pretrain/hard.sh
+++ scripts/experiments/pretrain/hard.sh
@@ -5,11 +5,12 @@ python src/main.py \
     tasks=["pretrain"] \
     hydra.run.dir=outputs/pretrain/hard \
     gpus=[1] \
-    max_time="01:00:00:00" \
+    max_time="03:00:00:00" \
     dataset.pretrain.batch_size=4 \
     dataset.pretrain.accumulate_grad_batches=4 \
     model.train.train_workers=8 \
     check_val_every_n_epoch=10 \
     model.optimizer.type=SGD \
     model.optimizer.lr=0.1 \
-    model.net.loss=hard
+    model.net.loss=hard \
+    pretrain_checkpoint=\"last.ckpt\"
