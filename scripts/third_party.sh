9b606e4b03a6306ddcfb770927db12f63d5932f4
andrej <andrej.janda@mail.utoronto.ca>
2022-01-19 16:44:55 -0500

    working on narval

--- scripts/third_party.sh
+++ scripts/third_party.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/outsidercsy/spconv.git src/packages/spconv --recursive
 
 # Clone Minkowski Engine for SLURM
-git clone  --depth 1 --branch v0.5.4 https://github.com/NVIDIA/MinkowskiEngine.git src/packages/MinkowskiEngine
+git clone  --depth 1 https://github.com/NVIDIA/MinkowskiEngine.git src/packages/MinkowskiEngine
+# git clone  --depth 1 --branch v0.5.4 https://github.com/NVIDIA/MinkowskiEngine.git src/packages/MinkowskiEngine
