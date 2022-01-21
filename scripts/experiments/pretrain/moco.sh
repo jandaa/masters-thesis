93b213f7b06ac66cb8ad892da593953ef153d309
andrej <andrej.janda@mail.utoronto.ca>
2022-01-21 13:11:43 -0500

    updates moco script

--- scripts/experiments/pretrain/moco.sh
+++ scripts/experiments/pretrain/moco.sh
@@ -1,15 +1,16 @@
 python src/main.py \
     dataset_dir=$dataset_dir \
     dataset=scannet \
+    dataset.name=scannetv2_pretrain \
     model=minkowski \
+    model.name=minkowski_moco \
     tasks=["pretrain"] \
     hydra.run.dir=outputs/pretrain/moco \
     gpus=[1] \
-    max_time="03:00:00:00" \
+    max_time="01:00:00:00" \
     dataset.pretrain.batch_size=4 \
     dataset.pretrain.accumulate_grad_batches=4 \
     model.train.train_workers=8 \
     check_val_every_n_epoch=10 \
     model.optimizer.type=SGD \
-    model.optimizer.lr=0.1 \
-    model.net.loss=mixing
+    model.optimizer.lr=0.1
