bf9b7f70d6a7b57dc91e28caa7bef9fc3e626d9a
andrej <andrej.janda@mail.utoronto.ca>
2022-01-24 16:02:01 -0500

    adds BYOL

---
+++ scripts/experiments/pretrain/byol.sh
@@ -0,0 +1,17 @@
+python src/main.py \
+    dataset_dir=$dataset_dir \
+    dataset=scannet \
+    dataset.name=scannetv2_pretrain \
+    model=minkowski \
+    model.name=minkowski_byol \
+    tasks=["pretrain"] \
+    hydra.run.dir=outputs/pretrain/byol \
+    gpus=[0] \
+    max_time="01:00:00:00" \
+    dataset.pretrain.batch_size=4 \
+    dataset.pretrain.accumulate_grad_batches=4 \
+    model.train.train_workers=8 \
+    check_val_every_n_epoch=10 \
+    model.optimizer.type=SGD \
+    model.optimizer.lr=0.1 \
+    model.net.model_n_out=64
