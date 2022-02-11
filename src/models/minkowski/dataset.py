c5c4d0c7521b3e6a9235ece1f1bbf86a37a99898
andrej <andrej.janda@mail.utoronto.ca>
2022-02-11 11:05:31 -0500

    fixes indexing bug

--- src/models/minkowski/dataset.py
+++ src/models/minkowski/dataset.py
@@ -243,7 +243,7 @@ class MinkowskiFrameDataset(SegmentationDataset):
             scene = pickle.load(scene_pickle)
 
         if scene.points.shape[0] == 0:
-            new_ind = random.randint(0, len(self.scenes))
+            new_ind = random.randint(0, len(self.scenes) - 1)
             return self[new_ind]
 
         xyz = np.ascontiguousarray(scene.points)
