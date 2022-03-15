2f246ecd7346d9cf22c7550a45bbd03ba9e7193c
andrej <andrej.janda@mail.utoronto.ca>
2022-03-15 17:04:16 -0400

    updates scripts

--- scripts/experiments/pretrain/images_transfer.sh
+++ scripts/experiments/pretrain/images_transfer.sh
@@ -5,10 +5,10 @@ python src/main.py \
     dataset_dir=$dataset_dir \
     dataset=scannet \
     dataset.name=scannetv2_pretrain_new \
-    dataset.pretrain.batch_size=16 \
+    dataset.pretrain.batch_size=8 \
     model=minkowski \
     model.name=minkowski_cme \
     model.train.train_workers=12 \
-    check_val_every_n_epoch=2 \
+    check_val_every_n_epoch=1 \
     dataset.classes=16 \
     pretrain_checkpoint_2d=\"epoch=31-step=18879-val_loss=6.38.ckpt\"
