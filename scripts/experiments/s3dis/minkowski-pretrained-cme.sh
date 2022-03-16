beb6bf718f25d55cbb2fa3c308903f6fb3df97f9
andrej <andrej.janda@mail.utoronto.ca>
2022-03-16 09:56:21 -0400

    updates cme script

--- scripts/experiments/s3dis/minkowski-pretrained-cme.sh
+++ scripts/experiments/s3dis/minkowski-pretrained-cme.sh
@@ -2,8 +2,9 @@ python src/main.py \
     dataset_dir=$dataset_dir \
     dataset=s3dis \
     model=minkowski \
+    model.name=minkowski_cme \
     tasks=["train","eval"] \
-    hydra.run.dir=$output_dir/pretrain/cme \
+    hydra.run.dir=$output_dir/pretrain/images \
     gpus=[0] \
     dataset.batch_size=6 \
     dataset.accumulate_grad_batches=8 \
@@ -12,4 +13,4 @@ python src/main.py \
     max_epochs=2000 \
     check_val_every_n_epoch=20 \
     model.train.train_workers=10 \
-    pretrain_checkpoint=\"epoch=33-step=39465-val_loss=0.63.ckpt\"
+    pretrain_checkpoint=\"epoch=3-step=18899-val_loss=3.70.ckpt\"
